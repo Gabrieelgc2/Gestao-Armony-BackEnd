99f7dd37f6881c1615ecacba49ee2cc9f0fee158
Gabrieelgc2 <gabrielao8@hotmail.com>
2026-08-05 11:05:42 -0300

    add: JWTGUARD

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/projects/project.module.ts
+++ src/projects/project.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard';
 
 @Module({
   imports: [],
   controllers: [ProjectsController],
-  providers: [ProjectsService, PrismaService],
+  providers: [ProjectsService, PrismaService, JwtAuthGuard],
 })
 export class ProjectModule {}
